edb9950f2ba2370fc9e0947b4cbb9b9db222d660
Yong Su Jeong <knarrblue@gmail.com>
2022-07-13 18:16:55 +0900

    FEAT - custom 유저 setting

--- gameduo/settings.py
+++ gameduo/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'gameduo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['MYSQL_DATABASE'],
+        'USER': os.environ['MYSQL_USER'],
+        'PASSWORD': os.environ['MYSQL_ROOT_PASSWORD'],
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -138,3 +142,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
