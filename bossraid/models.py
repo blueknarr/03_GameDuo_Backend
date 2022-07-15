be9025546521c3a8ce90938f89e002fe0ed02cce
Yong Su Jeong <knarrblue@gmail.com>
2022-07-15 23:14:23 +0900

    FEAT - Total Score 및 uuid 추가

--- bossraid/models.py
+++ bossraid/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from user.models import User
 
@@ -5,14 +7,25 @@ from user.models import User
 # Create your models here.
 class RaidsHistory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    raids_id = models.BigIntegerField('Boss Raids Id')
+    raids_id = models.UUIDField('Boss Raids Id', default=uuid.uuid4, editable=False)
     level = models.IntegerField('Level')
     score = models.IntegerField('Score')
     enter_time = models.DateTimeField('Enter Time')
-    end_time = models.DateTimeField('End Time')
+    end_time = models.DateTimeField('End Time', auto_now=True)
 
     class Meta:
         db_table = 'tb_raids_history'
 
     def __str__(self):
         return f'Id: {self.id}, BossRaids ID: {self.raids_id}, Level: {self.level}, Score: {self.score}'
+
+
+class RaidsTotalScore(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_score = models.IntegerField(default=0)
+
+    class Meta:
+        db_table= 'tb_total_score'
+
+    def __str__(self):
+        return f'Id: {self.id}, Total Score: {self.total_score}'
